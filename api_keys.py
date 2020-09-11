02537a2e481979fbceced7519d409f9ec44827f9
Gabriel Pivaro <gabrconc@gmail.com>
2020-09-11 18:30:24 -0500

    All set to start

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+with open("/etc/config.json") as config_file:
+    config = json.load(config_file)
+
+# OpenWeatherMap API Key
+weather_api_key == config.get("OPENWEATHERMAP_API_KEY")
+
+# Google API Key
+g_key = config.get("OPENWEATHERMAP_API_KEY")
