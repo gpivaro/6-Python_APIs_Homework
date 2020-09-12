5390332a08fd34a8270f43bdd2e2df7c24911245
Gabriel Pivaro <gabrconc@gmail.com>
2020-09-12 00:20:44 -0500

    Cleaned data ready

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,8 +1,10 @@
+import json
+
 with open("/etc/config.json") as config_file:
     config = json.load(config_file)
 
 # OpenWeatherMap API Key
-weather_api_key == config.get("OPENWEATHERMAP_API_KEY")
+weather_api_key = config.get("OPENWEATHERMAP_API_KEY")
 
 # Google API Key
-g_key = config.get("OPENWEATHERMAP_API_KEY")
+g_key = config.get("GOOGLE_API_KEY")
